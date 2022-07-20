597b89c38cc250a999b3853a9e1be8a86b82f3a8
pattytaborda <105998929+pattytaborda@users.noreply.github.com>
2022-07-19 23:40:08 -0500

    Add files via upload

---
+++ storage/framework/views/31859e63de6881c2e48ea8f6d0a00b3341317493.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" value="<?php echo e(csrf_token()); ?>"/>
+        <title>Laravel + Vue + Crud</title>
+
+        <!-- CDNs de Bootstrap 5 -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+        <!-- CDNs de Font-Awesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+        <link href="<?php echo e(mix('css/app.css')); ?>" type="text/css" rel="stylesheet"/>
+    </head>
+    <body>
+        <div id="app">
+
+        </div>
+        <script src="<?php echo e(mix('js/app.js')); ?>" type="text/javascript"></script>
+    </body>
+</html>
+<?php /**PATH E:\mvc-crud\resources\views/app.blade.php ENDPATH**/ ?>
